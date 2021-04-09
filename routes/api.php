487ac2eb6d299ac0518604bb2fff096e7ff9dfe3
younhong <younhong@kakao.com>
2021-04-09 17:06:22 +0900

    login

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 // Route::resource('products', ProductController::class);
 
 // Public routes
+Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::get("/products", [ProductController::class, 'index']);
 Route::get("/products/{id}", [ProductController::class, 'show']);
